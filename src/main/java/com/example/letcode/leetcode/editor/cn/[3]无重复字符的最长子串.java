937588e1872c352b4f9ec4f22470a975def05c72
李家旭 <lijiaxu@b2cf.cn>
2023-12-19 22:12:49 +0800

    无重复字符串的最长子串， 滑动窗口问题

---
+++ src/main/java/com/example/letcode/leetcode/editor/cn/[3]无重复字符的最长子串.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> window = new HashMap<>();
+
+        int left =0, right = 0;
+        int res = 0;
+
+        while (right<s.length()) {
+            char c = s.charAt(right);
+            right ++;
+            //如果这个Map中有这个值窗口的value 就+1
+            window.put(c,window.getOrDefault(c,0)+1);
+           //大于1说明找到重复的值了
+            while (window.get(c)>1) {
+                char d = s.charAt(left);
+                left++;
+                window.put(d,window.getOrDefault(d,0)-1);
+
+            }
+            res = Math.max(res,right-left);
+
+
+        }
+
+        return  res;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
